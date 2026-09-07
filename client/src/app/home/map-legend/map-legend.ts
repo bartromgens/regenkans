3022b21f3ed46061ba9010a1a79b19ab3ccd2931
Bart Römgens <bart.romgens@gmail.com>
2026-09-07 09:06:29 +0200

    Improve legend title layout

--- client/src/app/home/map-legend/map-legend.ts
+++ client/src/app/home/map-legend/map-legend.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { OverlayMode } from '../../radar/radar.service';
 
 @Component({
@@ -8,4 +8,15 @@ import { OverlayMode } from '../../radar/radar.service';
 })
 export class MapLegend {
   readonly mode = input.required<OverlayMode>();
+
+  readonly legendLabel = computed(() => {
+    switch (this.mode()) {
+      case 'probability':
+        return 'Regenkans in procent';
+      case 'expected':
+        return 'Verwachte intensiteit in millimeter per uur';
+      default:
+        return 'Regenintensiteit in millimeter per uur';
+    }
+  });
 }
